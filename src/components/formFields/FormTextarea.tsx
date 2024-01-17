3695e113fbb3a1773d2588e7bff82dbbac6bb943
Eray Demirel <113133576+ED-LetsCode@users.noreply.github.com>
2024-01-17 15:20:57 +0100

    Update FormTextarea.tsx

--- src/components/formFields/FormTextarea.tsx
+++ src/components/formFields/FormTextarea.tsx
@@ -9,7 +9,6 @@
 // } from "@/components/ui/form";
 // import { Textarea } from "@/components/ui/textarea";
 // import { Hint } from "@/components/Hint";
-// import { TextArea } from "@arkulpa/ui";
 
 // interface FormTextareaProps
 //   extends React.InputHTMLAttributes<HTMLTextAreaElement> {
